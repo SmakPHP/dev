66f621414f3b4adb1f98533d45861c204426d46d
Andrey Masterov <apmasterov@gmail.com>
2024-02-23 16:00:16 +0300

    Update d12.sh

--- d12.sh
+++ d12.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Execute bash script from link
-# curl -s https://.../d12.txt | bash -s arg1 arg2
+# curl https://raw.githubusercontent.com/smakphp/dev/main/d12.sh | bash -s arg1 arg2
 
 # Editing in nano editor
 # CTRL + O / CTRL + X
 
-# > /etc/resolv.conf
 echo "nameserver 192.168.2.1"
+# > /etc/resolv.conf
 
-# >> /etc/network/interfaces
+
 echo "
 auto enp1s0
 iface enp1s0 inet static
@@ -18,10 +18,11 @@ iface enp1s0 inet static
   gateway 192.168.2.1
   dns-nameservers 192.168.2.1
 "
+# >> /etc/network/interfaces
 
-systemctl restart networking
+# systemctl restart networking
 
 # Display double quotes in echo in unix > \"
-# > /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian bookworm contrib main
 "
+# > /etc/apt/sources.list
