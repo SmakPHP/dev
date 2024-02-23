91a7f337751c56a361a67f4fe8a04ca98234080b
Andrey Masterov <apmasterov@gmail.com>
2024-02-23 20:12:34 +0300

    Update d12.sh

--- d12.sh
+++ d12.sh
@@ -42,11 +42,11 @@ curl ipinfo.io/ip; echo
 # Privoxy http://p.p/
 #
 
-apt install privoxy
+# apt install privoxy
 
-sed -i 's/^listen-address  1.*/listen-address  192.168.1.1/g' /etc/privoxy/config
-sed -i 's/^#log-messages   1.*/log-messages   1/g' /etc/privoxy/config
-sed -i 's/^#debug     1.*/debug     1/g' /etc/privoxy/config
+# sed -i 's/^listen-address  1.*/listen-address  192.168.1.1/g' /etc/privoxy/config
+# sed -i 's/^#log-messages   1.*/log-messages   1/g' /etc/privoxy/config
+# sed -i 's/^#debug     1.*/debug     1/g' /etc/privoxy/config
 
-systemctl restart privoxy
-systemctl enable privoxy
+# systemctl restart privoxy
+# systemctl enable privoxy
