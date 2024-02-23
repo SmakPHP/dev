0cb975a6d57c33a61547e6dfc755b3fb85e369ad
Andrey Masterov <apmasterov@gmail.com>
2024-02-23 20:09:54 +0300

    Update d12.sh

--- d12.sh
+++ d12.sh
@@ -38,3 +38,15 @@ echo "deb http://deb.debian.org/debian bookworm contrib main
 
 curl ipinfo.io/ip; echo
 
+#
+# Privoxy http://p.p/
+#
+
+apt install privoxy
+
+sed -i 's/^listen-address  1.*/listen-address  192.168.1.1/g' /etc/privoxy/config
+sed -i 's/^#log-messages   1.*/log-messages   1/g' /etc/privoxy/config
+sed -i 's/^#debug     1.*/debug     1/g' /etc/privoxy/config
+
+systemctl restart privoxy
+systemctl enable privoxy
