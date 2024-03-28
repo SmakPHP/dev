30ceb47dce6a68ef453a8eb86e87fd32f8e3bbea
Andrey Masterov <apmasterov@gmail.com>
2024-03-28 21:55:02 +0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# systemctl --type=service --state=running
+
+systemctl stop cups
+systemctl disable cups
+systemctl mask cups
+
+systemctl stop cups-browsed
+systemctl disable cups-browsed
+systemctl mask cups-browsed
+
+systemctl stop avahi-daemon
+systemctl disable avahi-daemon
+systemctl mask avahi-daemon
