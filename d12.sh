f517022fb08b229b41968266e66b97ad0eadd2b1
Andrey Masterov <sddkg@ya.ru>
2024-12-29 14:15:47 +0600

    Update d12.sh

--- d12.sh
+++ d12.sh
@@ -4,13 +4,11 @@
 # wget -O - https://raw.githubusercontent.com/smakphp/dev/main/d12.sh | bash
 
 apt update -y
-apt upgrade -y
-
 apt install htop atop mc -y
 
 sed -i 's/^#*ClientAliveInterval.*/ClientAliveInterval 3600/g' /etc/ssh/sshd_config
 sed -i 's/^#*ClientAliveCountMax.*/ClientAliveCountMax 3/g' /etc/ssh/sshd_config
-
+systemctl restart sshd
 
 """
 
