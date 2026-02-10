10b83797602703c5e13708c7e2a23712739bcaae
Andrey Masterov <apmasterov@gmail.com>
2026-02-10 13:13:46 +0300

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
-# Network Manager
-# systemctl --type=service --state=running
+# Полезный софт
+apt install xchm
 
+# Ссылки для скачивания
 # apt install ./name.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Просмотр установленных пакетов
+apt list --installed | grep python
+# Удаление программ
+apt purge --auto-remove chromium-browser-stable
+
+# Список запущенных сервисов
+systemctl --type=service --state=running
+
+# Отключение лишнего
+systemctl mask --now cups
+systemctl mask --now avahi-daemon
+systemctl mask --now avahi-daemon.socket
+systemctl mask --now wpa_supplicant
+systemctl mask --now ufw
+
+# Просмотр первых/последних 10 строк
+head -10 /root/file.csv
+tail -10 /root/file.csv
+
+# Сводная информация
+inxi -p -m -s -B
+
+# Диск
+lsblk
+
+
+
+
+
 echo "
 # deb https://deb.debian.org/debian bookworm contrib main non-free-firmware
 deb [arch=amd64 trusted=yes] file:/home/go/debian12.5 bookworm contrib main
 # " > /etc/apt/sources.list
-
-systemctl stop cups
-systemctl disable cups
-systemctl mask cups
-
-systemctl stop cups-browsed
-systemctl disable cups-browsed
-systemctl mask cups-browsed
-
-systemctl stop avahi-daemon
-systemctl disable avahi-daemon
-systemctl mask avahi-daemon
 
 echo "# nameserver 192.168.2.1" > /etc/resolv.conf
 
